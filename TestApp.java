f8561750829255b7bc218dc501d65cf5b3708ef3
bheema101 <39403363+bheema101@users.noreply.github.com>
2021-04-24 21:00:19 +0530

    Add  Functionality
    Signed-off-by: bheema101 <39403363+bheema101@users.noreply.github.com>

--- TestApp.java
+++ TestApp.java
@@ -6,8 +6,10 @@ public class TestApp {
         public static void main(String[] args) {
 			
 			int a =100;
-			int c = 200;
-			omt z = 300;
+			int b = 200;
+			int z = 300;
+			int c = 400;
+			int k = 2332;
 			b = a++ + b++;
 			
 			if(b %2 == 0) {
